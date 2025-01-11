76385d2a6e220f31bd10d2746272becf77ac9183
mainframe98 <k.s.werf@hotmail.com>
2025-01-11 13:53:58 +0100

    Define a performance budget
    Change-Id: I3b53e23308647b118717b2f84ce15cd228ae0431

---
+++ tests/phpunit/Structure/BundleSizeTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace MediaWiki\Extension\Gravatar\Tests\Structure;
+
+use MediaWiki\Extension\Gravatar\GravatarLookup;
+use MediaWiki\Tests\Structure\BundleSizeTestBase;
+
+/**
+ * @coversNothing
+ */
+class BundleSizeTest extends BundleSizeTestBase {
+	/**
+	 * @before
+	 *
+	 * Mock GravatarLookup to prevent it from doing lookups.
+	 */
+	protected function replaceGravatarLookupWithMockSetUp(): void {
+		$this->setService( 'GravatarLookup', $this->createMock( GravatarLookup::class ) );
+	}
+
+	/** @inheritDoc */
+	public function getBundleSizeConfig(): string {
+		return __DIR__ . '/../../../bundlesize.config.json';
+	}
+}
